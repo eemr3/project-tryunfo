2b6d6c7eaa21c3f0ad127260947d1fc04d845b13
Emerson <eemr3@yahoo.com.br>
2021-11-06 16:29:36 -0300

    build(App): implementa funcionalidade que renderiza os cards após salvar  requisito-8

--- src/App.js
+++ src/App.js
@@ -114,6 +114,7 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      newDeck,
     } = this.state;
 
     return (
@@ -147,6 +148,22 @@ class App extends React.Component {
             cardTrunfo={ cardTrunfo }
           />
         </section>
+        <div className="list-card">
+          {this.onSaveButtonClick
+          && newDeck.map((card) => (
+            <Card
+              key={ card.cardName }
+              cardName={ card.cardName }
+              cardDescription={ card.cardDescription }
+              cardAttr1={ card.cardAttr1 }
+              cardAttr2={ card.cardAttr2 }
+              cardAttr3={ card.cardAttr3 }
+              cardImage={ card.cardImage }
+              cardRare={ card.cardRare }
+              cardTrunfo={ card.cardTrunfo }
+            />
+          ))}
+        </div>
       </main>
     );
   }
